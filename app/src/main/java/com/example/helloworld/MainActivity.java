118dc5ea6d95a2dd54cdab7b72d833369c3e5356
daviZ30 <davidzambrana86@gmail.com>
2023-09-26 10:05:33 +0200

    Version sin aplicar .gitignore

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvMessage;
+    private TextView textV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         tvMessage = findViewById(R.id.tvMessage);
+         textV = findViewById(R.id.textView);
+         tvMessage.setTextColor(getColor(R.color.white));
+         textV.setText(R.string.messageOptimistText);
+    }
+}
